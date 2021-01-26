2f9e52a42b5fb7f8a199f948c79b37ea7db21aa9
Anahide Tchertchian <atchertchian@nuxeo.com>
2021-01-26 15:23:08 +0100

    NXP-29984: migrate ServerLocatorService to use xmap registries

--- modules/runtime/nuxeo-runtime-management/src/main/java/org/nuxeo/runtime/management/ServerLocatorService.java
+++ modules/runtime/nuxeo-runtime-management/src/main/java/org/nuxeo/runtime/management/ServerLocatorService.java
@@ -20,9 +20,7 @@ package org.nuxeo.runtime.management;
 
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
-import java.net.InetAddress;
 import java.net.MalformedURLException;
-import java.net.UnknownHostException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.util.HashMap;
@@ -34,74 +32,51 @@ import javax.management.ObjectName;
 import javax.management.remote.JMXServiceURL;
 import javax.management.remote.rmi.RMIConnectorServer;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.nuxeo.runtime.model.ComponentInstance;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.runtime.model.ComponentContext;
 import org.nuxeo.runtime.model.DefaultComponent;
 
 public class ServerLocatorService extends DefaultComponent implements ServerLocator {
 
+    private static final Logger log = LogManager.getLogger(ServerLocatorService.class);
+
     public static final String LOCATORS_EXT_KEY = "locators";
 
-    private static final Log log = LogFactory.getLog(ServerLocatorService.class);
+    protected static final MBeanServer DEFAULT_SERVER = ManagementFactory.getPlatformMBeanServer();
 
-    protected final Map<String, MBeanServer> servers = new HashMap<>();
+    protected Map<String, MBeanServer> servers;
 
-    protected MBeanServer defaultServer = ManagementFactory.getPlatformMBeanServer();
-
-    protected String hostname;
+    protected MBeanServer defaultServer;
 
     @Override
-    public void registerContribution(Object contribution, String extensionPoint, ComponentInstance contributor) {
-        if (extensionPoint.equals(LOCATORS_EXT_KEY)) {
-            doRegisterLocator((ServerLocatorDescriptor) contribution);
-        }
+    public void start(ComponentContext context) {
+        servers = new HashMap<>();
+        this.<ServerLocatorDescriptor> getRegistryContributions(LOCATORS_EXT_KEY).forEach(descriptor -> {
+            MBeanServer server = descriptor.isExisting ? doFindServer(descriptor.domainName)
+                    : doCreateServer(descriptor);
+            servers.put(descriptor.domainName, server);
+            if (descriptor.isDefault) {
+                defaultServer = server;
+            }
+        });
     }
 
     @Override
-    public void unregisterContribution(Object contribution, String extensionPoint, ComponentInstance contributor) {
-        if (extensionPoint.equals(LOCATORS_EXT_KEY)) {
-            doUnregisterLocator((ServerLocatorDescriptor) contribution);
-        }
-    }
-
-    protected void doRegisterLocator(ServerLocatorDescriptor descriptor) {
-        MBeanServer server = descriptor.isExisting ? doFindServer(descriptor.domainName) : doCreateServer(descriptor);
-        servers.put(descriptor.domainName, server);
-        if (descriptor.isDefault) {
-            defaultServer = server;
-        }
-    }
-
-    protected String doGetHostname() {
-        if (hostname != null) {
-            return hostname;
-        }
-        try {
-            InetAddress addr = InetAddress.getLocalHost();
-            hostname = addr.getHostName();
-        } catch (UnknownHostException e) {
-            hostname = "localhost";
-        }
-        return hostname;
-    }
-
-    protected JMXServiceURL doFormatServerURL(ServerLocatorDescriptor descriptor) {
-        try {
-            return new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:" + descriptor.rmiPort + "/"
-                    + descriptor.domainName + "/jmxrmi");
-        } catch (MalformedURLException e) {
-            throw new ManagementRuntimeException("Cannot format url for " + descriptor.domainName);
-        }
-    }
-
-    protected String doFormatThreadName(ServerLocatorDescriptor descriptor) {
-        return "mbeanServer-" + descriptor.domainName;
+    public void stop(ComponentContext context) throws InterruptedException {
+        servers = null;
+        defaultServer = DEFAULT_SERVER;
     }
 
     protected MBeanServer doCreateServer(final ServerLocatorDescriptor descriptor) {
         MBeanServer server = MBeanServerFactory.createMBeanServer();
-        JMXServiceURL url = doFormatServerURL(descriptor);
+        JMXServiceURL url;
+        try {
+            url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:" + descriptor.rmiPort + "/"
+                    + descriptor.domainName + "/jmxrmi");
+        } catch (MalformedURLException e) {
+            throw new ManagementRuntimeException("Cannot format url for " + descriptor.domainName);
+        }
         if (!descriptor.remote) {
             return server;
         }
@@ -126,11 +101,10 @@ public class ServerLocatorService extends DefaultComponent implements ServerLoca
             }
         }
         assert connector.isActive();
-        log.info("Started a mbean server : " + url);
+        log.info("Started a mbean server: {}", url);
         return server;
     }
 
-    @SuppressWarnings("cast")
     protected MBeanServer doFindServer(String domainName) {
         for (MBeanServer server : MBeanServerFactory.findMBeanServer(null)) {
             String domain = server.getDefaultDomain();
@@ -142,15 +116,7 @@ public class ServerLocatorService extends DefaultComponent implements ServerLoca
         return defaultServer;
     }
 
-    protected void doUnregisterLocator(ServerLocatorDescriptor descriptor) {
-        servers.remove(descriptor.domainName);
-        if (descriptor.isDefault) {
-            defaultServer = ManagementFactory.getPlatformMBeanServer();
-        }
-    }
-
     @Override
-    @SuppressWarnings("cast")
     public MBeanServer lookupServer(ObjectName qualifiedName) {
         if (defaultServer.isRegistered(qualifiedName)) {
             return defaultServer;
@@ -171,10 +137,6 @@ public class ServerLocatorService extends DefaultComponent implements ServerLoca
     @Override
     public MBeanServer lookupServer(String domainName) {
         return doFindServer(domainName);
-    }
-
-    public void registerLocator(String domain, boolean isDefault) {
-        doRegisterLocator(new ServerLocatorDescriptor(domain, isDefault));
     }
 
 }
